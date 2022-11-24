4210063142b881a68c55130d9c9195bb5303bd3f
the69nemo <thestigmata@yandex.ru>
2022-11-24 21:30:52 +0300

    some_fix_+_menu

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -4,7 +4,9 @@ import "./ProductCard.css";
 function ProductCard({ title, img, prise }) {
   return (
     <div className="card">
-      <img className="card__img" src={img} alt="cup of cofee" />
+      <div className="card__img-wrapper">
+        <img className="card__img" src={img} alt="cup of cofee" />
+      </div>
       <div className="card__text-wrapper">
         <div className="card__title-container">
           <h2 className="card__title">{title}</h2>
